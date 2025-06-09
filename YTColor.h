cc54f0b66c7113f6fc32767d7a35ae1cb75b1a16
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2025-06-09 02:30:52 -0500

    Update YTColor.h

--- YTColor.h
+++ YTColor.h
@@ -2,17 +2,17 @@
 
 @interface YTColor : UIColor
 + (instancetype)colorWithRGB:(unsigned int)rgb floatAlpha:(CGFloat)alpha;
-+ (UIColor *)white1;
-+ (UIColor *)white2;
-+ (UIColor *)white3;
-+ (UIColor *)white4;
-+ (UIColor *)white5;
-+ (UIColor *)black0;
-+ (UIColor *)black1;
-+ (UIColor *)black2;
-+ (UIColor *)black3;
-+ (UIColor *)black4;
++ (instancetype *)white1;
++ (instancetype *)white2;
++ (instancetype *)white3;
++ (instancetype *)white4;
++ (instancetype *)white5;
++ (instancetype *)black0;
++ (instancetype *)black1;
++ (instancetype *)black2;
++ (instancetype *)black3;
++ (instancetype *)black4;
 + (instancetype)grey3Alpha70;
 + (instancetype)grey4Alpha60;
-+ (UIColor *)youTubeRed;
++ (instancetype *)youTubeRed;
 @end
