0fe4148ce69cab1afb8040dbc9532a0aff0b1be2
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-06-14 02:16:20 -0500

    Update YTICompactLinkRenderer.h

--- YTICompactLinkRenderer.h
+++ YTICompactLinkRenderer.h
@@ -3,7 +3,7 @@
 #import "YTICompactListItemThumbnailSupportedRenderers.h"
 
 @interface YTICompactLinkRenderer : GPBMessage
-@property (nonatomic, strong) YTIIcon *icon;
+@property (nonatomic, strong, readwrite) YTIIcon *icon;
 @property (nonatomic, strong) YTIFormattedString *title;
 @property (nonatomic, strong) YTICompactListItemThumbnailSupportedRenderers *thumbnail;
 - (BOOL)hasIcon;
