cd62f2be15368528938d344739e9f9e7044b23c2
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-06-14 02:03:52 -0500

    Update HAMFormatDescription.h

--- HAMFormatDescription.h
+++ HAMFormatDescription.h
@@ -1,6 +1,9 @@
 #import "HAMBuildableObject.h"
 #import "HAMFormat.h"
+#import <CoreMedia/CoreMedia.h>
 
 @interface HAMFormatDescription : HAMBuildableObject
 @property (nonatomic, assign, readonly) id <HAMFormat> format;
+- (CMFormatDescriptionRef)formatDescription;
+- (CMVideoCodecType)mediaSubType;
 @end
