62c24be3dfcaec60b7bda3868d84616b22e069e6
PoomSmart <3608783+PoomSmart@users.noreply.github.com>
2026-07-28 21:17:59 +0700

    Update

--- MLHAMQueuePlayer.h
+++ MLHAMQueuePlayer.h
@@ -1,8 +1,10 @@
 #import "MLHAMPlayer.h"
+#import "MLHAMPlayerItem.h"
 #import "MLPlayerDelegate.h"
 
 @interface MLHAMQueuePlayer : MLHAMPlayer
 @property (nonatomic, weak, readwrite) id <MLPlayerDelegate> delegate;
+- (MLHAMPlayerItem *)currentPlayerItem;
 - (void)internalSetRate;
 - (void)maybeSwitchToAVPlayer;
 @end
